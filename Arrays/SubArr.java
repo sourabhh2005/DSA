f9cd2a1c574fe381f57534260dc964f52f7a4d7d
Sourabh Singh <sourabhsingh8005@gmail.com>
2025-08-02 03:33:12 +0530

    commit

--- Arrays/SubArr.java
+++ Arrays/SubArr.java
@@ -3,17 +3,25 @@ public class SubArr {
   public static void subArrays(int[] arr) {
     int maxSum = Integer.MIN_VALUE;
     int minSum = Integer.MAX_VALUE;
+    int prefixSum[] = new int[arr.length];
+    prefixSum[0] = arr[0];
+    //CAL prefix sum
+    for (int i = 1; i < prefixSum.length; i++) {
+      prefixSum[i] = prefixSum[i - 1] + arr[i];
+    }
 
     for (int i = 0; i < arr.length; i++) {
       int sum = 0;
       for (int j = i; j < arr.length; j++) {
         sum += arr[j];
 
+        sum = i == 0 ? prefixSum[j] : prefixSum[j] - prefixSum[i - 1];
+
         // Print current subarray
-        for (int k = i; k <= j; k++) {
-          System.out.print(arr[k] + " ");
-        }
-        System.out.println();
+        // for (int k = i; k <= j; k++) {
+        //   System.out.print(arr[k] + " ");
+        // }
+        // System.out.println();
 
         // Update max and min
         maxSum = Math.max(maxSum, sum);
